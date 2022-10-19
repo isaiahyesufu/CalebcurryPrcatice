33cd99d80ae2bc3f02511d513d554c3d33394805
Yesufu Isaiah <anjolayesufu@gmail.com>
2022-10-19 14:33:37 +0100

    .

--- src/peersonalpractice1.java
+++ src/peersonalpractice1.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class peersonalpractice1 {
 
-        //this is on comparison operators and loical operators
+        //this is on comparison operators and logical operators
         public static void main(String[]args){
             System.out.println("Where do you live?");
             Scanner scanner= new Scanner(System.in);
             String location= scanner.nextLine();
             //String Gbagada= "Gbagada";
-            System.out.println("How old are you?");
-            int age  =scanner.nextInt();
-            System.out.println(location + (" ") +age);
+            System.out.println("What is your house number?");
+            int HouseNumber =scanner.nextInt();
+            System.out.println(location + (" ") +HouseNumber);
 
-            if( age>17 && (location.equals("Gbagada"))){
-                System.out.println("There would be a flood in Gbagada");
+            if(HouseNumber>12 && (location.equals("Gbagada"))){
+                System.out.println("There would be a flood around your house.Be careful!");
 
             }
             else{
