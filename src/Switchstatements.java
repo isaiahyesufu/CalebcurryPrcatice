38fd0a9f829610f9b179df34d50035f43350fe5b
Yesufu Isaiah <anjolayesufu@gmail.com>
2022-10-24 10:14:22 +0100

    src/Switchstatements.java

--- src/Switchstatements.java
+++ src/Switchstatements.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Switchstatements {
     public static void main (String[]args){
+        
         System.out.println("what state do you live?");
         Scanner scanner= new Scanner(System.in);
        String reply=scanner.nextLine();
@@ -12,6 +13,10 @@ public class Switchstatements {
            case "Lagos":
                System.out.println("Come back when your population has decreased");
                break;
+           default:
+               System.out.println("You should be either from Anambara or lagos to use this feature. Check our other website for yours.");
+               break;
+
 
         }
 
